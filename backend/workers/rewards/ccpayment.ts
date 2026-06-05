d3e9f68a9354b6d710ca3e70cc0abf2dae4ab0e7
unknown <christian-white@hotmail.com>
2026-06-05 07:57:35 -0500

    added images to crypto payments

--- backend/workers/rewards/ccpayment.ts
+++ backend/workers/rewards/ccpayment.ts
@@ -8,7 +8,13 @@ const PREDEFINED_CRYPTOS: PredefinedCrypto[] = [
   {
     rewardID: 'sol',
     rewardName: 'Solana',
-    description: 'Redeem your Sparks for Solana (SOL).',
+    description: 'Solana is a high-performance blockchain platform that enables fast and scalable decentralized applications.',
+    image: [
+      {
+        src: 'https://imagedelivery.net/oHD0oLOHVpmE-9Vp-RXuVg/7bd3cdbe-0484-4ebe-d65c-2444f0e3aa00/public',
+        type: 'card',
+      },
+    ],
     disclosure: '',
     countries: [],
     categories: [ 'crypto' ],
@@ -24,7 +30,13 @@ const PREDEFINED_CRYPTOS: PredefinedCrypto[] = [
   {
     rewardID: 'ltc',
     rewardName: 'Litecoin',
-    description: 'Redeem your Sparks for Litecoin (LTC).',
+    description: 'Litecoin is a peer-to-peer electronic cash system that is decentralized without a central authority.',
+    image: [
+      {
+        src: 'https://imagedelivery.net/oHD0oLOHVpmE-9Vp-RXuVg/997af3dd-1460-46de-5555-8915a0f13a00/public',
+        type: 'card',
+      },
+    ],
     disclosure: '',
     countries: [],
     categories: [ 'crypto' ],
